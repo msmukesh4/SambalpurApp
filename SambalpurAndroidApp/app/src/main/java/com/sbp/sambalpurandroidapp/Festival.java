dd754ffc98d6f5ecc43f55cb7bbc47bd020e4864
msmukesh4 <msmukesh4@gmail.com>
2016-01-14 02:27:27 +0530

    Checking if app has some updates available during Splash Screen

--- SambalpurAndroidApp/app/src/main/java/com/sbp/sambalpurandroidapp/Festival.java
+++ SambalpurAndroidApp/app/src/main/java/com/sbp/sambalpurandroidapp/Festival.java
@@ -3,15 +3,20 @@ package com.sbp.sambalpurandroidapp;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Created by mukesh on 3/1/16.
  */
 public class Festival extends AppCompatActivity {
 
+    ListView festival_list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_festival);
+        festival_list = (ListView) findViewById(R.id.festival_list);
+
     }
 }
--- SambalpurAndroidApp/app/src/main/java/com/sbp/sambalpurandroidapp/UpdateActivity.java
+++ SambalpurAndroidApp/app/src/main/java/com/sbp/sambalpurandroidapp/UpdateActivity.java
@@ -1,8 +1,28 @@
 package com.sbp.sambalpurandroidapp;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 /**
  * Created by mukesh on 13/1/16.
  */
-public class UpdateActivity {
-    
+public class UpdateActivity extends AppCompatActivity {
+
+    Button btn_update;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update);
+
+        btn_update = (Button) findViewById(R.id.btn_update);
+        btn_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Updating",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 }
